631c7dc7cc3a9c03a302e86e300cf285117f375f
e-sollier <etienne.sollier@gmail.com>
2024-03-18 09:18:25 +0100

    Add tkinter option

--- filedialpy/__init__.py
+++ filedialpy/__init__.py
@@ -8,7 +8,9 @@ if sys.platform=="linux":
     else:
         kdialog_path=shutil.which("kdialog")
         if kdialog_path is not None: from filedialpy.linux_kdialog import *
-        else: raise Exception("On linux, easier zenity or kdialog must be installed in order to use filedialpy.")
+        else: 
+            # use tkinter filedialogs if neither zenity nor kdialog are available.
+            from filedialpy.linux_tkinter import *
 elif sys.platform=="win32":
     from filedialpy.windows import *
 else:
---
+++ filedialpy/linux_tkinter.py
@@ -0,0 +1,47 @@
+import subprocess
+import os
+from tkinter import Tk
+import tkinter.filedialog
+
+
+
+def convert_filter(filter):
+    if filter is None: return []
+    if isinstance(filter,str): filter=[filter]
+    new_filters=[]
+    for x in filter:
+        new_filters.append((x,x.replace("*","")))
+    return new_filters
+
+def openFile(initial_dir=None,initial_file=None,filter=None,title=None):
+    root = Tk()
+    root.withdraw()
+    print(convert_filter(filter))
+    t=tkinter.filedialog.askopenfilename(initialdir=initial_dir,initialfile=initial_file,title=title,filetypes=convert_filter(filter))
+    if len(t)==0: t=""
+    root.destroy()
+    return t
+
+def openFiles(initial_dir=None,initial_file=None,filter=None,title=None):
+    root = Tk()
+    root.withdraw()
+    t=tkinter.filedialog.askopenfilenames(initialdir=initial_dir,initialfile=initial_file,title=title,filetypes=convert_filter(filter))
+    if len(t)==0: t=""
+    root.destroy()
+    return t
+
+def openDir(initial_dir=None,initial_file=None,filter=None,title=None):
+    root = Tk()
+    root.withdraw()
+    t=tkinter.filedialog.askdirectory(initialdir=initial_dir,initialfile=initial_file,title=title,filetypes=convert_filter(filter))
+    if len(t)==0: t=""
+    root.destroy()
+    return t
+
+def saveFile(initial_dir=None,initial_file=None,filter=None,title=None):
+    root = Tk()
+    root.withdraw()
+    t=tkinter.filedialog.asksaveasfilename(initialdir=initial_dir,initialfile=initial_file,title=title,filetypes=convert_filter(filter))
+    if len(t)==0: t=""
+    root.destroy()
+    return t
